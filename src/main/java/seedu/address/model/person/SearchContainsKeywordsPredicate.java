1dc35ad51576a888eba0a75a3c477958533a8f50
Zhu-Jiahui <jiahuizhu1@gmail.com>
2018-03-15 13:24:01 +0800

    Replace NameContainsKeywords to SearchContainsKeywords since now the find function searches all fields

---
+++ src/main/java/seedu/address/model/person/SearchContainsKeywordsPredicate.java
@@ -0,0 +1,37 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class SearchContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public SearchContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword)) ||
+                keywords.stream()
+                        .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword)) ||
+                keywords.stream()
+                        .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword)) ||
+                keywords.stream()
+                        .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SearchContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((SearchContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
