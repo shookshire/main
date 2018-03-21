15fac566813f6065796c43c426589682ce412b52
shookshire <destinnsy@gmail.com>
2018-03-21 22:09:55 +0800

    Added Client Class

---
+++ src/main/java/seedu/address/model/person/Client.java
@@ -0,0 +1,47 @@
+package seedu.address.model.person;
+
+import seedu.address.model.tag.Tag;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+public class Client extends Person {
+
+    private final ArrayList<Location> location;
+    private final ArrayList<Level> level;
+    private final ArrayList<Subject> subject;
+
+    /**
+     * Every field must be present and not null.
+     * @TODO: include location, subject and level field when add method is updated
+     */
+    public Client(Name name, Phone phone, Email email, Address address, Set<Tag> tags, ArrayList<Location> location, ArrayList<Level> level, ArrayList<Subject> subject) {
+        super(name, phone, email, address, tags);
+        requireAllNonNull(location, level, subject);
+        this.location = location;
+        this.level = level;
+        this.subject = subject;
+    }
+
+    public boolean hasLocation(Location toCheck) {
+        return location.contains(toCheck);
+    }
+
+    public boolean hasLevel(Level toCheck) {
+        return level.contains(toCheck);
+    }
+
+    public boolean hasSubject(Subject toCheck) {
+        return subject.contains(toCheck);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(this.getName(), this.getPhone(), this.getEmail(), this.getAddress(), this.getTags(), location, level, subject);
+    }
+    
+}
