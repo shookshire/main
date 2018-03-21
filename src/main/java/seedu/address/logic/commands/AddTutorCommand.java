d45ef9629916e9d202c7dd3d4f9a3dd1bfb87ac6
shookshire <destinnsy@gmail.com>
2018-03-21 23:13:56 +0800

    added AddTutorCommand

---
+++ src/main/java/seedu/address/logic/commands/AddTutorCommand.java
@@ -0,0 +1,71 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.Client;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+
+/**
+ * Adds a tutor to TuitionCor.
+ */
+public class AddTutorCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "addtutor";
+    public static final String COMMAND_ALIAS = "at";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a tutor to TuitionCor. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_TAG + "TAG]... "
+            + PREFIX_LOCATION + "LOCATION "
+            + PREFIX_GRADE + "GRADE "
+            + PREFIX_SUBJECT + "SUBJECT\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_TAG + "friends "
+            + PREFIX_TAG + "owesMoney "
+            + PREFIX_LOCATION + "east "
+            + PREFIX_GRADE + "pri6 "
+            + PREFIX_SUBJECT + "physics";
+
+    public static final String MESSAGE_SUCCESS = "New tutor added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This tutor already exists in the address book";
+
+    private final Client toAdd;
+
+    /**
+     * Creates an AddTutorCommand to add the specified {@code Client}
+     */
+    public AddTutorCommand(Client client) {
+        requireNonNull(client);
+        toAdd = client;
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        requireNonNull(model);
+        try {
+            model.addTutor(toAdd);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+        } catch (DuplicatePersonException e) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddTutorCommand // instanceof handles nulls
+                && toAdd.equals(((AddTutorCommand) other).toAdd));
+    }
+}
