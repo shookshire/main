842e4b3804a14544ce980903bcde258186441808
shookshire <destinnsy@gmail.com>
2018-03-21 22:54:04 +0800

    Added grade location and subject prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser;
 
+
 /**
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
@@ -11,5 +12,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_LOCATION = new Prefix("l/");
+    public static final Prefix PREFIX_GRADE = new Prefix("g/");
+    public static final Prefix PREFIX_SUBJECT = new Prefix("s/");
 
 }
