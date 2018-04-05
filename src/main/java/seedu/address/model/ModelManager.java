4497fb0544d070207fd6e41b92cc961610ebec58
Zhu-Jiahui <jiahuizhu1@gmail.com>
2018-04-05 19:13:58 +0800

    Collate code

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -228,6 +228,7 @@ public class ModelManager extends ComponentManager implements Model {
     }
     //=========== Ranked Person List Accessors =============================================================
 
+    //@@author Zhu-Jiahui
     /**
      * Returns an unmodifiable view of the list of {@code Client} backed by the internal list of
      * {@code addressBook}
@@ -287,6 +288,7 @@ public class ModelManager extends ComponentManager implements Model {
         }
 
     }
+    //@@author
 
     @Override
     public ObservableList<Client> getFilteredClosedTutorList() {
